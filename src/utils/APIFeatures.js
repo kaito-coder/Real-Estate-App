463525d67a2a04650ed8253f948b9f6d28155ae5
kaito-coder <kaitok57a01@gmail.com>
2023-02-20 09:49:25 +0700

    [tvbCtJU6]feat: Add middlewares for api features

---
+++ src/utils/APIFeatures.js
@@ -0,0 +1,52 @@
+const APIFeatures = class {
+  // querytring = req.query;
+  // query = model.find();
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+    return this;
+  }
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+  paginate() {
+    const pageDefault = 1;
+    const limitDefault = 10;
+    const page = this.queryString.page * 1 || pageDefault;
+    const limit = this.queryString.limit * 1 || limitDefault;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+};
+module.exports = APIFeatures;
