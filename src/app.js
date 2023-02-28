b42bae72849f7b371d79d1e53454749eaf4d4136
kaito-coder <kaitok57a01@gmail.com>
2023-02-28 04:34:17 +0000

    [DoLSphez]Feat:Add authentication and authorization : register , login , reset update password

--- src/app.js
+++ src/app.js
@@ -2,17 +2,49 @@ import express from 'express';
 import morgan from 'morgan';
 import AppError from './utils/AppError.js';
 import globalErrorHandler from './controllers/errorController.js';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+import hpp from 'hpp';
+import userRouter from './routers/userRoutes.js';
+
 const app = express();
 
-// MIDDLEWARE
+// GLOBAL MILLDEWARE
+// Set security HTTP headers
+app.use(helmet());
+//Development looging
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+//Limit request from the same API
+const limiter = rateLimit({
+  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many request from this IP , please try again in an hour',
+});
+app.use('/api', limiter);
+
+// Body parser,reading data from into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitiation against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+// Prevent parameter pollution
+app.use(hpp());
+
+//test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+// ROUTER HANDLERS
+app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
--- src/models/user.js
+++ src/models/user.js
@@ -1,32 +1,40 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bycrypt from 'bcryptjs';
+import crypto from 'crypto';
 
 const { Schema } = mongoose;
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: [true, 'username is required'],
-    },
-    firstName: {
-      type: String,
-      required: [true, 'First name is required'],
-    },
-    lastName: {
-      type: String,
-      required: [true, 'Last name is required'],
-    },
-    email: {
-      type: String,
-      required: [true, 'Email is requried'],
-      unique: true,
-      trim: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error('Invalid email');
-        }
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'please tell us your first name'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'please tell us your last name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'please provide a valid email'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'please provide a valid email'],
+  },
+  password: {
+    type: String,
+    minLengh: [8, 'password must be at least 8 characters long'],
+    required: [true, 'Password is required'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: true,
+    validate: {
+      // This only works with CREATE & SAVE!!!!!
+      validator: function (el) {
+        return el === this.password;
       },
     },
     password: {
@@ -71,10 +79,66 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
   },
-  {
-    timestamps: true,
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+  passwordChangedAt: {
+    type: Date,
+  },
+  address: {
+    type: String,
+  },
+  phoneNumber: {
+    type: String,
+    maxLength: 10,
+  },
+  profileImage: {
+    type: String,
+  },
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+});
+userSchema.pre('save', async function (next) {
+  // only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+  // hash the password with cost of 12
+  this.password = await bycrypt.hash(this.password, 12);
+  //delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+});
+
+userSchema.methods.correctPassword = async function (
+  canidatePassword,
+  userPassword
+) {
+  return await bycrypt.compare(canidatePassword, userPassword);
+};
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return JWTTimestamp < changedTimestamp;
   }
-);
+  return false;
+};
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  return resetToken;
+};
 
 const UserModel = mongoose.model('Users', userSchema);
 export default UserModel;
