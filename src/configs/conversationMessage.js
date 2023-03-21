a2e5fd13030c4bb7a72b6ecff4ebc57230ef82cd
kaito-coder <kaitok57a01@gmail.com>
2023-03-21 10:36:35 +0700

    [NYhl3EWW]feat:API delete message in conversation

--- src/configs/conversationMessage.js
+++ src/configs/conversationMessage.js
@@ -1,8 +1,11 @@
 export const conversationError = {
   notFound: 'Owner Estate not found',
   success: 'Conversation created successfully',
+  reCall: 'Message recall successfully',
   availableConversation: 'Retrieve exist conversation',
   createSuccess: 'creating a new conversation',
   OK: 'Success',
   notInConversation: 'user not in conversation',
+  404: 'Not found',
+  errorUserNotOwner: 'You are not the owner of this message',
 };
--- src/middlewares/userModelMiddlewares.js
+++ src/middlewares/userModelMiddlewares.js
@@ -2,9 +2,10 @@ import catchAsync from '../utils/catchAsync.js';
 import AppError from '../utils/AppError.js';
 import status from 'http-status';
 import { message } from '../configs/commentMessageRes.js';
-
 import ConversationModel from '../models/conversation.js';
 import { conversationError } from '../configs/conversationMessage.js';
+import ConversationMessageModel from '../models/conversationMessage.js';
+
 export const setEstateUserIds = catchAsync(async (req, res, next) => {
   if (!req.body?.estate) req.body.estate = req.params.estateId;
   if (!req.body?.user) req.body.user = req.user.id;
@@ -14,11 +15,22 @@ export const setEstateUserIds = catchAsync(async (req, res, next) => {
 export const checkIfUserIsOwner = (Model) =>
   catchAsync(async (req, res, next) => {
     const checkModel = await Model.findById(req.params.id);
-    if (!checkModel.user.toString() === req.user.id) {
+    if (!(checkModel.user.toString() === req.user.id)) {
       return next(new AppError(message.errorUserNotOwner, status.UNAUTHORIZED));
     }
     next();
   });
+export const checkIfUserIsOwnerMessage = catchAsync(async (req, res, next) => {
+  const conversationMessage = await ConversationMessageModel.findById(
+    req.params.conversationMessageId
+  );
+  if (!(conversationMessage.postedByUser.toString() === req.user.id)) {
+    return next(
+      new AppError(conversationError.errorUserNotOwner, status.UNAUTHORIZED)
+    );
+  }
+  next();
+});
 export const checkUserIsInConversation = catchAsync(async (req, res, next) => {
   const currentConversation = await ConversationModel.findById(
     req.params.conversationId
